f89e2a20efb06ec67f68488569c2e0c3eaaf2e52
wooldridge <mike@hellion.org>
2022-08-22 19:21:09 -0700

    Config updates

--- ui/src/config/search.config.js
+++ ui/src/config/search.config.js
@@ -4,65 +4,6 @@ const searchConfig  = {
     search: {
         "defaultEntity": "person",
     
-        "meter": {
-            "component": "SummaryMeter",
-            "config": {
-                "colors": {
-                    "all": "#cccccc",
-                    "filters": "#1ACCA8"
-                },
-                "totalPath": "searchResults.recordCount.total"
-            }
-        },
-    
-        "facets": {
-            "component": "Facets",
-            "config": {
-                "selected": "#1acca8",
-                "unselected": "#dfdfdf",
-                "displayThreshold": 3,
-                "displayShort": 3,
-                "displayLong": 5,
-                "items": [
-                {
-                    "type": "dateRange",
-                    "name": "created",
-                    "tooltip": "Filter by date created."
-                },
-                {
-                    "type": "dateRange",
-                    "name": "updated",
-                    "tooltip": "Filter by date updated."
-                },
-                {
-                    "type": "string",
-                    "name": "sources",
-                    "tooltip": "Filter by source."
-                },
-                {
-                    "type": "string",
-                    "name": "status",
-                    "tooltip": "Filter by status."
-                },
-                {
-                    "type": "string",
-                    "name": "country",
-                    "tooltip": "Filter by country."
-                },
-                {
-                    "type": "string",
-                    "name": "area",
-                    "tooltip": "Filter by area."
-                }
-                ]
-            }
-        },
-    
-        "selectedFacets": {
-            "component": "SelectedFacets",
-            "config": {}
-        },
-    
         "results": {
             "component": "ResultsList",
             "config": {
@@ -70,16 +11,9 @@ const searchConfig  = {
                     "path": "entityType",
                     "rootRelative": false
                 },
-                "pageLengths": [10, 20, 40, 80],
                 "defaultIcon" : {
                     "type": "faCircle",
                     "color": "lightgrey"
-                },
-                "sort":{
-                    "entities": ["person", "organization"],
-                    "label": "Created On",
-                    "sortBy": "createdOn",
-                    "order": "descending"
                 },
                 "entities": {
                     "person": {
@@ -196,96 +130,6 @@ const searchConfig  = {
                                     "color": "color",
                                     "url": "url"
                                 }
-                            }
-                        }
-                    },
-                    "organization": {
-                        "icon": {
-                            "type": "faIndustry",
-                            "color": "#fdbcc6"
-                        },
-                        "thumbnail": {
-                            "component": "Image",
-                            "config": {
-                                "arrayPath": "extracted.organization.images.image",
-                                "path": "url",
-                                "alt": "result thumbnail",
-                                "style": {
-                                    "width": "70px",
-                                    "height": "70px"
-                                }
-                            }
-                        },
-                        "title": {
-                            "id": {
-                                "path": "uri"
-                            },
-                            "component": "Value",
-                            "config": {
-                                "arrayPath": "extracted.organization.names",
-                                "path": "name.value"
-                            }
-                        },
-                        "items": [
-                        {
-                            "component": "Value",
-                            "config": {
-                            "path": "extracted.organization.types.type"
-                            }
-                        },
-                        {
-                            "component": "Value",
-                            "config": {
-                            "path": "extracted.organization.country"
-                            }
-                        },
-                        {
-                            "component": "Value",
-                            "config": {
-                            "path": "extracted.organization.areas.area"
-                            }
-                        },
-                        {
-                            "component": "Concat",
-                            "config": {
-                                "items": [
-                                    "Created by: ",
-                                    {
-                                        "path": "extracted.organization.createdOn.user"
-                                    },
-                                    {
-                                        "path": "extracted.organization.createdOn.ts",
-                                        "type": "DateTime",
-                                        "prefix": " (",
-                                        "suffix": ")",
-                                    }
-                                ]
-                            }
-                        }
-                        ],
-                        "categories": {
-                            //"arrayPath": "extracted.organization.sources",
-                            //"path": "source.name",
-                            "path": "$.extracted.organization.sources..*[?(@property === 'name')]",
-                            // Filter out all categories NOT ("New York Times" OR "Wall Street Journal")
-                            // "path": "$.extracted.organization.sources..*[?(@property === 'name' && (@ === 'New York Times' || @ === 'Wall Street Journal'))]",
-                            "colors": {
-                                "New York Times": "#d5e1de",
-                                "USA Today": "#ebe1fa",
-                                "Los Angeles Times": "#cae4ea",
-                                "Wall Street Journal": "#fae9d3",
-                                "Washington Post": "#fae3df",
-                                "Chicago Tribune": "#f0f6d9"
-                            }
-                        },
-                        "timestamp": {
-                            "arrayPath": "extracted.organization.createdOn",
-                            "path": "ts",
-                            "type": "datetime",
-                            "format": "yyyy-MM-dd",
-                            "prefix": "Created on ",
-                            "style": {
-                                "fontStyle": "normal"
                             }
                         }
                     }
