00e9a06a55986888bb5faea20f5f6dbdb926dfd1
wooldridge <mike@hellion.org>
2022-09-20 17:21:13 -0700

    Make ReactiveSearch UI configurable between Elasticsearch and MarkLogic backends

--- ui-reactive/src/index.js
+++ ui-reactive/src/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // React pre-18
 import { render } from "react-dom";
-import { ReactiveBase, DataSearch, ReactiveList, ResultCard } from "@appbaseio/reactivesearch";
+
+import { ReactiveBase, DataSearch, ReactiveList, MultiList } from "@appbaseio/reactivesearch";
 import appConfig from './config/app.config';
-import searchConfig from './config/searchML.config.js';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.scss";
+
 import ResultsList from './components/ResultsList/ResultsList';
+import searchConfig from './config/search.config.js'; // Elasticsearch UI config
+import searchConfigML from './config/searchML.config.js'; // MarkLogic UI config
 
 // React 18
 // const el = document.getElementById('root');
@@ -17,16 +21,22 @@ import ResultsList from './components/ResultsList/ResultsList';
 // React pre-18
 const root = document.getElementById("root");
 
+const dataSource = "MarkLogic"; // "Elasticsearch" or "MarkLogic"
+
 const App = () => {
 
     const name = appConfig.name || "Search App";
 
     return (
         <ReactiveBase
-            url="http://localhost:3000"
+            url={ dataSource ==="Elasticsearch" ? "http://localhost:3000" : "http://localhost:7777" }
             app="good-books-ds"
             credentials="elastic:X1loFwNN3fFJLAOCjtRn"
             enableAppbase={false}
+            transformResponse={async (elasticsearchResponse, componentId) => {
+                console.log("transformResponse", elasticsearchResponse, componentId);
+                return elasticsearchResponse;
+            }}
         >
             <div>
                 <header style={{backgroundColor: "rgb(43, 51, 60)", width: "100%", padding: "10px"}}>
@@ -34,74 +44,89 @@ const App = () => {
                     <div class="searchbox">
                         <DataSearch
                             componentId="searchbox"
-                            dataField={[
-                            {
-                                "field": "authors",
-                                "weight": 3
-                            },
-                            {
-                                "field": "authors.autosuggest",
-                                "weight": 1
-                            },
-                            {
-                                "field": "original_title",
-                                "weight": 5
-                            },
-                            {
-                                "field": "original_title.autosuggest",
-                                "weight": 1
-                            },
-                            ]}
-                            placeholder="Search for books or authors"
+                            dataField={ dataSource === "Elasticsearch" ? [
+                                {
+                                    "field": "authors",
+                                    "weight": 3
+                                },
+                                {
+                                    "field": "authors.autosuggest",
+                                    "weight": 1
+                                },
+                                {
+                                    "field": "original_title",
+                                    "weight": 5
+                                },
+                                {
+                                    "field": "original_title.autosuggest",
+                                    "weight": 1
+                                },
+                            ] : "content.person.nameGroup.fullname.value" }
+                            placeholder="Search for data"
                             innerClass={{
                                 input: 'search-input'
                             }}
                         />
                     </div>
                 </header>
-                <div style={{padding: '10px'}}>
-                    {/* <ReactiveList
-                        componentId="results"
-                        dataField="_score"
-                        size={10}
-                        pagination={true}
-                        react={{
-                        and: ["searchbox"] //, "authorsfilter", "ratingsfilter"]
-                        }}
-                        render={({ data }) => (
-                        <ReactiveList.ResultCardsWrapper>
-                        {data.map((item) => (
-                            <ResultCard key={item._id}>
-                            <ResultCard.Image src={item.image} />
-                            <ResultCard.Title
-                            dangerouslySetInnerHTML={{
-                            __html: item.original_title
-                            }}
-                            />
-                            <ResultCard.Description>
-                            {item.authors + " " + "*".repeat(item.average_rating)}
-                            </ResultCard.Description>
-                            </ResultCard>
-                        ))}
-                        </ReactiveList.ResultCardsWrapper>
-                        )}
-                    /> */}
-                    <ReactiveList
-                        componentId="results2"
-                        dataField="_score"
-                        size={6}
-                        pagination={true}
-                        showResultStats={true}
-                        react={{
+                <div style={{display: 'flex', alignItems: 'flex-start'}}>
+                    <div id="left" style={{padding: '10px', minWidth: '300px', display: 'inline-block'}}>
+                        <MultiList
+                            componentId="list-0"
+                            dataField={ dataSource === "Elasticsearch" ? "authors.keyword" : "country" }
+                            className="filter"
+                            title="Filter"
+                            filterLabel="Filter"
+                            size={10}
+                            sortBy="count"
+                            react={{ and: ["searchbox"]}}
+                        />
+                    </div>
+                    <div style={{padding: '10px', display: 'inline-block', width: 'calc(100% - 340px)'}}>
+                        {/* <ReactiveList
+                            componentId="results"
+                            dataField="_score"
+                            size={10}
+                            pagination={true}
+                            react={{
                             and: ["searchbox"] //, "authorsfilter", "ratingsfilter"]
-                        }}
-                        render={({ data }) => (
-                            <ResultsList 
-                                data={data} 
-                                config={ searchConfig.search.results.config } 
-                            />
-                        )}
-                    />
+                            }}
+                            render={({ data }) => (
+                            <ReactiveList.ResultCardsWrapper>
+                            {data.map((item) => (
+                                <ResultCard key={item._id}>
+                                <ResultCard.Image src={item.image} />
+                                <ResultCard.Title
+                                dangerouslySetInnerHTML={{
+                                __html: item.original_title
+                                }}
+                                />
+                                <ResultCard.Description>
+                                {item.authors + " " + "*".repeat(item.average_rating)}
+                                </ResultCard.Description>
+                                </ResultCard>
+                            ))}
+                            </ReactiveList.ResultCardsWrapper>
+                            )}
+                        /> */}
+                        <ReactiveList
+                            componentId="results2"
+                            dataField="_id"
+                            size={10}
+                            pagination={true}
+                            showResultStats={true}
+                            react={{
+                                and: ["searchbox", "list-0"] //, "authorsfilter", "ratingsfilter"]
+                            }}
+                            render={({ data }) => (
+                                <ResultsList 
+                                    data={data} 
+                                    config={ dataSource === "Elasticsearch" ? 
+                                        searchConfig.search.results.config : searchConfigML.search.results.config } 
+                                />
+                            )}
+                        />
+                    </div>
                 </div>
             </div>
         </ReactiveBase>
